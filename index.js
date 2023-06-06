48ff7eb2eb203ab0528c6b849e551493c5c80b81
J3fte <127363441+J3fte@users.noreply.github.com>
2023-06-06 11:14:31 -0600

    Update index.js
    Update to POO

--- index.js
+++ index.js
@@ -1,67 +1,74 @@
-
-function shoot(n){
-    var pine=Math.round(Math.random() * n)
-    return pine
-}
-var arrayPrincipal = []
-function implementacion(){
-    for(var i = 0; i<10; i++){
-        var arraysecunadrio =[]
-        for(var j =0; j<4 ; j++){
-            if(j == 0){ //Se  hace el primer tiro 
-                tiro = shoot(10) //Como es el primer tiro todos lo bolos estan arriba
-                arraysecunadrio.push(tiro)  //Se agregar el valor al array
-            }else if(j == 1){ //Se hace el segundo tiro
-                if(arraysecunadrio[0]<10){//si no se hace un strike se hace el siguiente tiro
-                    tiro = shoot(10-arraysecunadrio[0])   
-                    arraysecunadrio.push(tiro)
-                   }
-                else{//Si se hace strike se reinician los bolos
-                    tiro = shoot(10)
-                    arraysecunadrio.push(tiro)
+class BowlingGame{
+    constructor(){
+        this.arrayPrincipal = []
+    }
+    shoot(thrownBowling){
+        return Math.round(Math.random() * thrownBowling)
+    }
+    Game(){
+        for (let i = 0; i < 10; i++) {
+            let arraysecunadrio = [];
+            for (let j = 0; j < 4; j++) {
+              if (j === 0) {
+                const tiro = this.shoot(10);
+                arraysecunadrio.push(tiro);
+              } else if (j === 1) {
+                if (arraysecunadrio[0] < 10) {
+                  const tiro = this.shoot(10 - arraysecunadrio[0]);
+                  arraysecunadrio.push(tiro);
+                } else {
+                  const tiro = this.shoot(10);
+                  arraysecunadrio.push(tiro);
                 }
+              } else if (j === 2) {
+                if (
+                  this.isSpare(arraysecunadrio[0], arraysecunadrio[1]) ||
+                  arraysecunadrio[0] === 10
+                ) {
+                  const tiro = this.shoot(10);
+                  arraysecunadrio.push(tiro);
+                } else {
+                  arraysecunadrio.push(0);
+                }
+              } else if (j === 3) {
+                let total;
+                if (i === 0) {
+                  const totalTiros = this.sumaTiros(
+                    arraysecunadrio[0],
+                    arraysecunadrio[1],
+                    arraysecunadrio[2]
+                  );
+                  arraysecunadrio.push(totalTiros);
+                } else {
+                  const totalTiros = this.sumaTiros(
+                    arraysecunadrio[0],
+                    arraysecunadrio[1],
+                    arraysecunadrio[2]
+                  );
+                  total = this.sumaTirosAnteriores(
+                    totalTiros,
+                    this.arrayPrincipal[i - 1][3]
+                  );
+                  arraysecunadrio.push(total);
+                }
+              }
             }
-            else if ( j == 2){//Cuando esta en la terscer tirada
-                if(IsSpare(arraysecunadrio[0],arraysecunadrio[1]) || arraysecunadrio[0]==10){ //si se hace un spare entonces se hace el bonus 
-                    tiro = shoot(10)
-                    arraysecunadrio.push(tiro)
-                }
-                else{//en el caso de que no sea chusa o ni strike se tiene que ag
-                    arraysecunadrio.push(0)
-                }
-            }else if(j == 3 ){
-                if(i == 0){
-                    totalTiros = sumaTiros(arraysecunadrio[0],arraysecunadrio[1],arraysecunadrio[2])
-                    arraysecunadrio.push(totalTiros)
-                }else{
-                    totalTiros = sumaTiros(arraysecunadrio[0],arraysecunadrio[1],arraysecunadrio[2])
-                    total = sumaTirosAnteriores(totalTiros , arrayPrincipal[i-1][3])
-                    arraysecunadrio.push(total)
-                    
-                }
-            }
+            this.arrayPrincipal.push(arraysecunadrio);
+          }
+          return this.arrayPrincipal;
         }
-        arrayPrincipal.push(arraysecunadrio)
+    isSpare(shoot1,shoot2){
+        return shoot1 +shoot2
     }
-    console.log(arrayPrincipal)
-}
-
-function IsSpare(shoot1,shoot2){
-    if((shoot1+shoot2)==10){
-        return true
-    }
+    sumaTiros(tiro1, tiro2, tiro3) {
+        return tiro1 + tiro2 + tiro3;
+      }
     
+    sumaTirosAnteriores(sumaAnterior, sumaActual) {
+        return sumaAnterior + sumaActual;
+    }
 }
 
-function sumaTiros(tiro1,tiro2,tiro3){
-    var suma = tiro1+tiro2+tiro3;
-    return suma
-}
 
-function sumaTirosAnteriores(sumaAnterior, SumaActual){
-    total = sumaAnterior + SumaActual
-    return total
-}
-
-console.log(implementacion())
-//implementacion()
+const bowlingGame = new BowlingGame()
+console.log(bowlingGame.Game());
